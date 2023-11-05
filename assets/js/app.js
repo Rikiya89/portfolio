c7e229abefaabad777083e5bba8a4f630f3af7c0
Rikiya89 <83910358+Rikiya89@users.noreply.github.com>
2023-11-05 14:32:25 +0900

    update_04

--- assets/js/app.js
+++ assets/js/app.js
@@ -89,23 +89,18 @@ window.addEventListener('scroll', function() {
     });
 });
 
-const swiper = new Swiper(".swiper", {
-    loop: true, // ループ
-    speed: 600, // 少しゆっくり(デフォルトは300)
-    mousewheel: true, // マウスホイールでスライド
-    direction: "vertical", // 縦方向
-    autoplay: { // 自動再生
-      delay: 3000, // 1秒後に次のスライド
-      disableOnInteraction: false, // 矢印をクリックしても自動再生を止めない
-    },
-    //ページネーション
-    pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    },
-    // 前後の矢印
-    // navigation: {
-    //   nextEl: ".swiper-button-next",
-    //   prevEl: ".swiper-button-prev",
-    // },
-});
+// const swiper = new Swiper(".swiper", {
+//     loop: true, // ループ
+//     speed: 900, // 少しゆっくり(デフォルトは300)
+//     mousewheel: true, // マウスホイールでスライド
+//     direction: "vertical", // 縦方向
+//     autoplay: { // 自動再生
+//       delay: 3000, // 1秒後に次のスライド
+//       disableOnInteraction: false, // 矢印をクリックしても自動再生を止めない
+//     },
+//     //ページネーション
+//     pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//     },
+// });
